94df5376efb69dfadad6bc50e6ec9f2f4e9a11a1
JenikAdamira <jenik.adamira@gmail.com>
2025-05-05 07:57:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 from docxtpl import DocxTemplate
 import io
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -27,3 +28,7 @@ def index():
         return send_file(output, as_attachment=True, download_name=filename)
 
     return render_template("form.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
