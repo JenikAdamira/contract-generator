8f7d3879bc3eb6619086191c2dee29d0b1f8dff9
JenikAdamira <jenik.adamira@gmail.com>
2025-05-14 20:57:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def index():
 
         cislo_akce_final = ", ".join(cisla_akci)
 
-        vice_akci = ""
+vice_akci = ""
         if akce_count >= 2:
-            vice_akci = "která se skládá ze dvou níže uvedených jednotlivých akcí:\n"
+            vice_akci = "Tato zakázka se skládá ze dvou níže uvedených jednotlivých akcí:\n"
             for cislo, nazev in zip(cisla_akci, nazvy_akci):
                 if cislo and nazev:
                     vice_akci += f"č. {cislo} {nazev}\n"
